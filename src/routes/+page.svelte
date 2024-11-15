2f99a4ca418697380216ea59d673ab8723452fdb
Ezio Tobioli <ezio.tobioli@bairesdev.com>
2024-11-15 09:47:54 -0300

    changes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="card p-4 bg-white shadow-lg rounded-lg text-center grid h-screen place-items-center">
+	<h3 class="h3">
+	<span class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">Ya estas cerca de encontrar tu repuesto.</span>
+	</h3>
+</div>
